10199199422cd67cbc57b1fbb786e11ce8a89bf3
louiegonzalez236-cmd <louiegonzalez236@gmail.com>
2026-07-20 18:14:11 -0500

    Add API folder and Axios service files

---
+++ src/api/reviewApi.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/reviews";
+
+export const getReviews = async () => {
+    const response = await asxios.get(API_URL);
+    return response.data;
+};
