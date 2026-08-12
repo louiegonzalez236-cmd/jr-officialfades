397f397aadc4ba41eaee8f50c244f38751ac34e0
louiegonzalez236-cmd <louiegonzalez236@gmail.com>
2026-08-12 16:42:00 -0500

    Add log in button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Hero from "./components/Hero";
 import Services from "./components/Services";
@@ -22,7 +22,34 @@ function App() {
         <Route
           path="/"
           element={
-            <div>
+            <div style={{ position: "relative" }}>
+              
+              {/* Small Admin Login Link in the top-right corner */}
+              <div
+                style={{
+                  position: "absolute",
+                  top: "20px",
+                  right: "20px",
+                  zIndex: 9999
+                }}
+              >
+                <Link
+                  to="/admin-login"
+                  style={{
+                    color: "#555",
+                    textDecoration: "none",
+                    fontSize: "14px",
+                    fontWeight: "600",
+                    padding: "6px 10px",
+                    borderRadius: "6px",
+                    background: "rgba(255,255,255,0.8)",
+                    boxShadow: "0 0 4px rgba(0, 0, 0, 0.15)"
+                  }}
+                >
+                  Admin Login
+                </Link>
+              </div>
+
               <Hero />
               <Services />
               <Menu />
@@ -56,4 +83,3 @@ function App() {
 }
 
 export default App;
-
