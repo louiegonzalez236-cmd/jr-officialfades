145fc7acefac75ec48b3359c1aade770ef766ee3
louiegonzalez236-cmd <louiegonzalez236@gmail.com>
2026-08-12 17:13:37 -0500

    fixed localhost to render

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -4,12 +4,17 @@ import axios from "axios";
 function AdminLogin() {
   const [form, setForm] = useState({ username: "", password: "" });
 
-  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", form);
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/auth/login`,
+        form
+      );
+
       localStorage.setItem("token", res.data.token);
       alert("Logged in successfully!");
       window.location.href = "/dashboard";
@@ -22,8 +27,19 @@ function AdminLogin() {
     <section>
       <h2>Admin Login</h2>
       <form onSubmit={handleSubmit}>
-        <input name="username" value={form.username} onChange={handleChange} placeholder="Username" />
-        <input name="password" type="password" value={form.password} onChange={handleChange} placeholder="Password" />
+        <input
+          name="username"
+          value={form.username}
+          onChange={handleChange}
+          placeholder="Username"
+        />
+        <input
+          name="password"
+          type="password"
+          value={form.password}
+          onChange={handleChange}
+          placeholder="Password"
+        />
         <button type="submit">Login</button>
       </form>
     </section>
@@ -31,3 +47,4 @@ function AdminLogin() {
 }
 
 export default AdminLogin;
+
