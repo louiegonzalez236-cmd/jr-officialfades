bef137b748a1c3b863447c441c4eb4e2bf86ebf5
louiegonzalez236-cmd <louiegonzalez236@gmail.com>
2026-08-12 18:15:30 -0500

    Changed localhost to render in appointments

--- src/api/appointmentApi.js
+++ src/api/appointmentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/appointments";
+const API_URL = `${process.env.REACT_APP_API_URL}/appointments`;
 
 export const createAppointment = async (appointmentData) => {
     const response = await axios.post(API_URL, appointmentData);
